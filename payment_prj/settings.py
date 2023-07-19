018d7aa76f45c81b45671ad11ae4be479ffbd585
MDZAID123 <88980148+MDZAID123@users.noreply.github.com>
2023-07-20 00:06:33 +0530

    configured aws s3 settings

--- payment_prj/settings.py
+++ payment_prj/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'core',
     'userauths',
     'account',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -167,3 +168,28 @@ JAZZMIN_SETTINGS = {
     # "order_with_respect_to": ["core","userauths","transactions","addon","blog"]
 
 }
+
+
+AWS_ACCESS_KEY_ID = "AKIA6GFD5BJDMG2JFP6S"
+
+AWS_SECRET_ACCESS_KEY = "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"
+
+AWS_STORAGE_BUCKET_NAME = "payliomyproject"
+
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_LOCATION = 'static'
+
+STATIC_LOCATION = 'static'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
