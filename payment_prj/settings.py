73068e7739c7bcad25b4e5fba81fb4ba2ae6d86e
MDZAID123 <88980148+MDZAID123@users.noreply.github.com>
2023-07-19 16:09:36 +0530

    Allowed applicaton to collect static files

--- payment_prj/settings.py
+++ payment_prj/settings.py
@@ -130,6 +130,8 @@ LOGOUT_REDIRECT_URL="userauths:sign-in"
 STATIC_URL='/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
